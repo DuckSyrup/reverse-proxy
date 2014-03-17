d1e2605e8d16af0c183846ce1d3b7e94b8b1af25
Manic <basbalmanic@gmail.com>
2014-03-17 00:04:42 -0700

    Added mongodb to gitignore

--- frontend.js
+++ frontend.js
@@ -2,11 +2,16 @@ var express = require('express');
 exp = express();
 exp.set('views', __dirname + '/www/views');
 exp.set('view engine', 'jade');
-exp.use(express.static(__dirname + '/www/public')) 
+exp.use(express.static(__dirname + '/www/public'))
 
+var db = require('./db_api');
 
-exp.get('/*', function(req, res){
+exp.get('/', function(req, res){
 	res.render('index',{title: 'Home'});
 });
+
+exp.get('/add'), function(req, res){
+	
+}
 
 exp.listen(8080);
