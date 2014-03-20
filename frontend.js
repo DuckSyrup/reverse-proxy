d98691d44133d58d099ad1424a5e53ec88c8c828
Manic <basbalmanic@gmail.com>
2014-03-19 22:39:00 -0700

    404 handling.

--- frontend.js
+++ frontend.js
@@ -1,6 +1,8 @@
 var express = require('express');
 exp = express();
 
+exp.use(exp.router);
+
 //We use www as the containing folder for all front-facing webserver files
 exp.use(express.static(__dirname + '/www/public'))
 exp.set('views', __dirname + '/www/views');
@@ -61,6 +63,25 @@ exp.post('/add', function(req, res) {
 		else
 			res.render('add', {worked: false, mess: 'Key and IP not received.', item:{key: 'UNDEFINED', ip: 'UNDEFINED'}});
 	}
+});
+
+exp.use(function(req, res, next){
+	res.status(404);
+	
+	// respond with html page
+	if (req.accepts('html')) {
+		res.render('404', { url: req.url });
+		return;
+	}
+	
+	// respond with json
+	if (req.accepts('json')) {
+		res.send({ error: 'Not found' });
+		return;
+	}
+	
+	// default to plain-text. send()
+	res.type('txt').send('Not found');
 });
 
 exp.listen(8080);
