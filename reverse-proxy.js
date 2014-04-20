bbb38e145fe234dd518af16a2fe9ffa1eddf3ad7
Manic <basbalmanic@gmail.com>
2014-04-20 02:30:59 -0700

    Fixed POST routing.

--- reverse-proxy.js
+++ reverse-proxy.js
@@ -4,7 +4,7 @@ process.on('uncaughtException', function(err) {
 	console.log('uncaught exception: ' + err);
 });
 
-var table = {};
+var table = {}; //Routing taable
 
 exports.init = function(options) {
     if (options.table) {
@@ -12,28 +12,36 @@ exports.init = function(options) {
     }
 }
 
+//Add a route to the routing table
 exports.addRoute = function(obj){
     table[obj.key] = obj.ip;
 }
 
+//Remove a route from the routing table
 exports.removeRoute = function(obj) {
     delete table[obj];
 }
 
-
+//GET a route
 exports.get = function(key, req, res, path){
     if (table[key]) {
         request.get('http://'+table[key] + path).pipe(res);
     }
 }
 
+//POST a route
 exports.post = function(key, req, res, path) {
     if (table[key]) {
+	//We rebuild the parsed request.  This could also be achieved by only using body parsing middleware on specific routes.
+	var body = "";
+	for (i in req.body) {
+		body += i + "=" + req.body[i] + "&"; //This outputs "key1=param1&key2=param2&"
+	}
+	body  = body.slice(0, - 1); //Get rid of the trailing ampersand
         var options = {
             url: 'http://'+table[key]+path,
 	    headers: req.headers,
-            body: req.body,
-            method: 'POST'
+            body: body
         }
         request.post(options).pipe(res);
     }
